a30cdfdf61bf321648893e35a96bffce2bc4eedd
Willem Melis <willem.melis@outlook.com>
2024-06-15 11:16:50 +0200

    Add cmake target for compiler frontend
    * Add cmd-args for file and HIL print.
    * Add some code to read and print out the source file.

---
+++ IR/compiler/main.cpp
@@ -0,0 +1,30 @@
+#include "llvm/Support/CommandLine.h"
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+using namespace llvm;
+
+cl::list<std::string>
+    SourceFilenames("i", cl::desc("Specify source filename"),
+                    cl::value_desc("filename"), cl::Required);
+cl::opt<bool> OutputHIL("printHIL", cl::desc("Print out the HIL after parsing"),
+                        cl::value_desc("printHIL"), cl::init(false));
+
+int main(int argc, char **argv) {
+  cl::ParseCommandLineOptions(argc, argv);
+
+  for (auto &SourceFilename : SourceFilenames) {
+    std::ifstream ifs(SourceFilename);
+    if (!ifs.good()) {
+        std::cout << "Can't read file with path=" << SourceFilename << "\n";
+    }
+
+    std::ostringstream oss;
+    oss << ifs.rdbuf();
+    std::string SourceFile = oss.str();
+    std::cout << SourceFile << "\n";
+  }
+
+  return 0;
+}
