56048ab5ba73c4141ec00c3a3ab69bcef9581d21
Willem Melis <willem.melis@outlook.com>
2024-07-14 13:49:34 +0200

    Cleanup tests
    * Removed the *.sh test file, and put the .spl files in the lit config as tests.

--- test/Parse/test.sh
+++
@@ -1,4 +0,0 @@
-# RUN: tokenizer %S/simple_func.spl > %t.tokens
-# RUN: diff %t.tokens %S/simple_func.tokens
-# RUN: cspl -i %S/simple_func.spl -o %t.mlir -HIL
-# RUN: diff %t.mlir %S/simple_func_mlir
